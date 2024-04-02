cea4cf8adf4d6ae5994a73e91caed7b90945e0d0
Kr0laS <balteromer12@gmail.com>
2024-04-03 00:50:49 +0300

    date picker.

--- EnviTech/ViewModel/MainViewModel.cs
+++ EnviTech/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using EnviTech.Db;
 
@@ -8,13 +9,37 @@ namespace EnviTech.ViewModel
     {
         private ObservableCollection<string> _dateList;
 
-         public ObservableCollection<string> DateList
+        public ObservableCollection<string> DateList
         {
             get { return _dateList; }
             set
             {
                 _dateList = value;
                 OnPropertyChanged(nameof(DateList));
+            }
+        }
+
+        private string _startDate;
+
+        public string StartDate
+        {
+            get { return _startDate; }
+            set
+            {
+                _startDate = value;
+                OnPropertyChanged(nameof(StartDate));
+            }
+        }
+
+        private string _endDate;
+
+        public string EndDate
+        {
+            get { return _endDate; }
+            set
+            {
+                _endDate = value;
+                OnPropertyChanged(nameof(EndDate));
             }
         }
 
